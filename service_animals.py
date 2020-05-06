3d1b1b580f8bee1bec4b2ca77bf334fe268c738e
Aidan Feldman <aidan.feldman@gmail.com>
2020-05-06 01:50:02 -0400

    generalize regulation data downloader

--- service_animals.py
+++ service_animals.py
@@ -3,18 +3,18 @@ import itertools
 import requests
 
 # https://beta.regulations.gov/document/DOT-OST-2018-0068-12959/comment
-REG_OBJ_ID = "090000648432b7e1"
+DOCKET_ID = "DOT-OST-2018-0068-12959"
 FILENAME = "service_animals.csv"
 
 
 # curl 'https://beta.regulations.gov/api/comments?filter%5BcommentOnId%5D=090000648432b7e1&page%5Bnumber%5D=1'
-def get_comments(page):
-    params = {"filter[commentOnId]": REG_OBJ_ID, "page[number]": page}
+def get_comments(docket_obj_id, page):
+    params = {"filter[commentOnId]": docket_obj_id, "page[number]": page}
     response = requests.get("https://beta.regulations.gov/api/comments", params=params)
     return response.json()
 
 
-def get_comment(id):
+def get_document(id):
     response = requests.get("https://beta.regulations.gov/api/documentdetails/" + id)
     data = response.json()["data"]
 
@@ -28,17 +28,20 @@ def get_comment(id):
     return attrs
 
 
+docket = get_document(DOCKET_ID)
+docket_obj_id = docket["objectId"]
+
 with open(FILENAME, "w") as csvfile:
     writer = None
 
     for page in itertools.count(start=1):
         print("Page", page)
-        response = get_comments(page)
+        response = get_comments(docket_obj_id, page)
 
         for comment_summary in response["data"]:
             id = comment_summary["id"]
             print(" ", id)
-            comment = get_comment(id)
+            comment = get_document(id)
 
             if not writer:
                 # initialize
