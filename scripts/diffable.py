7a04645d5364fbad9ef5104e03ec6e58f7241225
Aidan Feldman <aidan.feldman@gmail.com>
2020-05-08 10:25:02 -0400

    fix placement of outputs check

--- scripts/diffable.py
+++ scripts/diffable.py
@@ -10,15 +10,15 @@ for cell in notebook["cells"]:
         # ignore all the execution count numbers
         cell["execution_count"] = None
 
+    if cell["cell_type"] != "code":
+        continue
+
     # filter out pip upgrade warnings
     cell["outputs"] = [
         line
         for line in cell["outputs"]
         if not re.match(r"WARNING: You are using pip version|--upgrade pip", line)
     ]
-
-    if cell["cell_type"] != "code":
-        continue
 
     for output in cell["outputs"]:
         # clear HTML output, since it often has generated IDs (from displacy, plotly, etc.) that change with each execution
