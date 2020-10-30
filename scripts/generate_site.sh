9f62fd81ce112ae7f2106a2b39f10b267fc5dedf
Aidan Feldman <aidan.feldman@gmail.com>
2020-10-30 06:56:52 -0400

    fix image display in site

--- scripts/generate_site.sh
+++ scripts/generate_site.sh
@@ -3,18 +3,21 @@
 set -e
 set -x
 
-
+TEMPLATE_DIR=scripts/templates
+TEMPLATE=ppp
 DEST=public
+
 rm -r "$DEST"
 mkdir "$DEST"
 
-cp -r scripts/templates/ppp/static public
+cp -r "$TEMPLATE_DIR/$TEMPLATE/static" public
+cp -r img public
 
 for notebook in *.ipynb
 do
   jupyter nbconvert --to html \
-    --TemplateExporter.extra_template_basedirs=scripts/templates \
-    --template ppp \
+    --TemplateExporter.extra_template_basedirs="$TEMPLATE_DIR" \
+    --template "$TEMPLATE" \
     --output-dir="$DEST" \
     "$notebook"
 done
