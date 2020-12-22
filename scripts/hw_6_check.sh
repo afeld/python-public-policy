dcb578e35b08906594109b249e8adf7a477fb8c0
Aidan Feldman <aidan.feldman@gmail.com>
2020-12-22 00:40:00 -0500

    intermediate save of HW6 check shell script

--- scripts/hw_6_check.sh
+++ scripts/hw_6_check.sh
@@ -6,11 +6,25 @@ set -e
 set -o pipefail
 # set -x
 
-FILE=$1
+NOTEBOOK=$1
+
+SCRIPT=$(jupyter nbconvert --to script --log-level WARN --stdout "$NOTEBOOK")
 
 LINES_OF_CODE=$(
-  jupyter nbconvert --to script --log-level WARN --stdout "$FILE" | \
+  echo "$SCRIPT" | \
   cloc --stdin-name=script.py --json - | \
   jq .SUM.code
 )
 echo "Lines of code: $LINES_OF_CODE"
+
+LINK_IN_MARKDOWN=$(jq '[.cells | .[] | select(.cell_type == "markdown") | .source] | flatten | any(. | test("https?://"))' "$NOTEBOOK") #| grep -E 'https?://'
+LINK_IN_COMMENT=$(jq '[.cells | .[] | select(.cell_type == "code") | .source] | flatten | any(. | test("^\s*#.*https?://"))' "$NOTEBOOK")
+
+# if echo "$SCRIPT" | grep -E '^\s*#.*https?://'; then
+if echo "$LINK_IN_MARKDOWN" == "true"; then
+  echo "Has link in Markdown"
+elif echo "$LINK_IN_COMMENT" == "true"; then
+  echo "Has link in comment"
+else
+  echo "Doesn't have link"
+fi
