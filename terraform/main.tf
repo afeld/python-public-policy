63c1fc0364f63d9b447c8b5c815113107bbdcdb3
Aidan Feldman <aidan.feldman@gmail.com>
2021-04-08 00:40:30 -0400

    add Terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  project = "python-public-policy"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "data" {
+  name     = "python-public-policy"
+  location = "US-CENTRAL1"
+}
+
+# make publicly readable
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.data.name
+  role   = "READER"
+  entity = "allUsers"
+}
