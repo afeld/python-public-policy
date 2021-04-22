6124e55ec31eb9f6ebfece0864b6f2a168e426a5
Aidan Feldman <aidan.feldman@gmail.com>
2021-04-22 01:45:24 -0400

    use bash function to centralize diffable command

--- scripts/check_change.sh
+++ scripts/check_change.sh
@@ -10,13 +10,17 @@ PRE="tmp/$SOURCE"
 POST=${PRE//\./.nbconvert.}
 FINAL=${PRE//\./.final.}
 
+function diffable() {
+  jupyter nbconvert --to notebook --Exporter.preprocessors=scripts.diffable.Diffable --stdout "$1" > "$2"
+}
+
 mkdir -p tmp
-jupyter nbconvert --to notebook --Exporter.preprocessors=scripts.diffable.Diffable --stdout "$SOURCE" > "$PRE"
+diffable "$SOURCE" "$PRE"
 
 cp "$PRE" "$POST"
 ./scripts/update.sh "$POST"
 
-jupyter nbconvert --to notebook --Exporter.preprocessors=scripts.diffable.Diffable --stdout "$POST" > "$FINAL"
+diffable "$POST" "$FINAL"
 
 echo "Comparing output..."
 DIFF=$(nbdiff "$PRE" "$FINAL")
