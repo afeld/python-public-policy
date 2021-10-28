8051a03bca4f464deb83594df6eecb4661455a8a
Aidan Feldman <aidan.feldman@gmail.com>
2021-10-28 15:54:16 -0400

    remove Colab badges

--- scripts/validate.py
+++ scripts/validate.py
@@ -23,17 +23,9 @@ def test_colab_metadata(notebook, file):
         ), f"Name in metadata doesn't match filename for {file}."
 
 
-def test_badge(notebook, file):
-    cell = notebook["cells"][0]
-    source = cell["source"][0]
-    nb_url = f"https://colab.research.google.com/github/afeld/python-public-policy/blob/main/{file}"
-    assert nb_url in source, f"'Open in Colab' badge missing/incorrect for {file}."
-
-
 for file in glob.glob("*.ipynb"):
     print(f"Checking {file}...")
     notebook = json.load(open(file))
     test_colab_metadata(notebook, file)
-    test_badge(notebook, file)
 
 print("Passed!")
