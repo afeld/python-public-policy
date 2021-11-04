998a6030d357d67bfae4330eee9c1a951dfa19d6
Aidan Feldman <aidan.feldman@gmail.com>
2021-11-04 15:12:37 -0400

    move as many things out of top-level directory as possible
    Will hopefully be less confusing to students.

--- extras/scripts/validate.py
+++ extras/scripts/validate.py
@@ -2,15 +2,9 @@ import glob
 import json
 
 
-def test_metadata(notebook, file):
+def test_metadata(notebook, file, expected_kernel):
     metadata = notebook["metadata"]
     runtime = metadata["kernelspec"]["display_name"]
-
-    if file == "pandas_crash_course.ipynb":
-        # run in Google Colab
-        expected_kernel = "Python 3"
-    else:
-        expected_kernel = "Python [conda env:python-public-policy] *"
 
     assert (
         runtime == expected_kernel
@@ -25,9 +19,16 @@ def test_metadata(notebook, file):
         ), f"Name in metadata doesn't match filename for {file}."
 
 
-for file in glob.glob("*.ipynb"):
+def test_file(file, expected_kernel="Python [conda env:python-public-policy] *"):
     print(f"Checking {file}...")
     notebook = json.load(open(file))
-    test_metadata(notebook, file)
+    test_metadata(notebook, file, expected_kernel)
+
+
+for file in glob.glob("*.ipynb"):
+    test_file(file)
+
+# run in Google Colab
+test_file("extras/pandas_crash_course.ipynb", "Python 3")
 
 print("Passed!")
