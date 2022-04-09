6d9429b7b790d6ce5b7fce97f870709efbdee402
Aidan Feldman <aidan.feldman@gmail.com>
2022-04-09 19:44:37 -0400

    refactor diffable into smaller functions
    …and add a few tests.

---
+++ extras/scripts/test_diffable.py
@@ -0,0 +1,14 @@
+from nbformat import NotebookNode
+from scripts.diffable import should_clear_output
+
+def test_ignore_empty():
+    cell = NotebookNode({"source": ""})
+    assert should_clear_output(cell) == False
+
+def test_ignore_random():
+    cell = NotebookNode({"source": "foo = 6"})
+    assert should_clear_output(cell) == False
+
+def test_ignore_system():
+    cell = NotebookNode({"source": "!pip install"})
+    assert should_clear_output(cell)
