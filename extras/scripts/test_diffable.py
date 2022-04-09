110da4b1ce32cdfa2f9a198a4532d49e72f5014e
Aidan Feldman <aidan.feldman@gmail.com>
2022-04-09 19:50:41 -0400

    add dummy outputs

--- extras/scripts/test_diffable.py
+++ extras/scripts/test_diffable.py
@@ -3,17 +3,17 @@ from scripts.diffable import should_clear_output
 
 
 def test_ignore_empty():
-    cell = NotebookNode({"source": ""})
+    cell = NotebookNode({"source": "", "outputs": ""})
     assert should_clear_output(cell) == False
 
 def test_ignore_random():
-    cell = NotebookNode({"source": "foo = 6"})
+    cell = NotebookNode({"source": "foo = 6", "outputs": ""})
     assert should_clear_output(cell) == False
 
 def test_ignore_system():
-    cell = NotebookNode({"source": "!pip install"})
+    cell = NotebookNode({"source": "!pip install", "outputs": ""})
     assert should_clear_output(cell)
 
 def test_ignore_ipytest():
-    cell = NotebookNode({"source": "%%ipytest -qq"})
+    cell = NotebookNode({"source": "%%ipytest -qq", "outputs": ""})
     assert should_clear_output(cell)
