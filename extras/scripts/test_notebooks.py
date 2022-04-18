5e18c8b64fa5bc428a066597b507f1f8284d3109
Aidan Feldman <aidan.feldman@gmail.com>
2022-04-17 22:53:24 -0700

    ensure H1s and H2s are slides and H3+ are subslides

--- extras/scripts/test_notebooks.py
+++ extras/scripts/test_notebooks.py
@@ -22,11 +22,16 @@ def check_file(file, expected_kernel="Python [conda env:python-public-policy] *"
     check_metadata(notebook, file, expected_kernel)
 
 
+def is_markdown(cell):
+    return cell.cell_type == "markdown"
+
+
 def is_h1(cell):
-    return cell.cell_type == "markdown" and cell.source.startswith("# ")
+    return is_markdown(cell) and cell.source.startswith("# ")
 
 
 notebooks = glob.glob("*.ipynb")
+notebooks.sort()
 crash_course = "extras/pandas_crash_course.ipynb"
 all_notebooks = notebooks + [crash_course]
 
@@ -46,3 +51,18 @@ def test_one_h1(file):
     notebook = read_notebook(file)
     num_h1s = sum(is_h1(cell) for cell in notebook.cells)
     assert num_h1s == 1
+
+
+@pytest.mark.parametrize("file", all_notebooks)
+def test_heading_levels(file):
+    notebook = read_notebook(file)
+    for cell in notebook.cells:
+        meta = cell.metadata
+        source = cell.source
+        if is_markdown(cell) and 'slideshow' in meta and source.startswith("#"):
+            # slide with a heading
+            slide_type = meta['slideshow']['slide_type']
+            if slide_type == 'slide':
+                assert source.startswith(("# ", "## ")), "should be an H1 or H2"
+            elif slide_type == 'subslide':
+                assert source.startswith("###"), "should be H3+"
