5caaaf39012cc56a96f11cb0e12bf52c7eafae00
Aidan Feldman <aidan.feldman@gmail.com>
2022-05-22 22:49:51 -0400

    simplify update script

--- extras/scripts/update_packages.sh
+++ extras/scripts/update_packages.sh
@@ -4,10 +4,7 @@ set -e
 set -x
 
 
-cd extras
-
 # https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#updating-an-environment
-mamba env update --file environment.yml --prune
+mamba env update --file extras/environment.yml --prune
 
-cd -
 ./extras/scripts/update_all.sh
