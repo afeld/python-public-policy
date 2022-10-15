3ac8c8f3711d9626878ea2ed89837adededec070
Aidan Feldman <aidan.feldman@gmail.com>
2022-10-15 03:46:19 -0400

    switch site's launch buttons to use synchronized repository URLs

---
+++ _static/site.js
@@ -0,0 +1,12 @@
+// JupyterBook's Launch of JupyterHub functionality generates a nbgitpuller URL:
+// https://jupyterbook.org/en/stable/interactive/launchbuttons.html#add-a-launch-on-jupyterhub-button
+// This script switches that link to use the notebook URL in the synchronized repository.
+window.addEventListener("load", () => {
+  const launcher = document.querySelector(".menu-dropdown-launch-buttons a");
+  if (launcher) {
+    // it's a page generated from a notebook
+
+    const nbName = window.location.pathname.match(/\/(\w+)\.html/)[1];
+    launcher.href = `https://padmgp-4506-spring.rcnyu.org/user-redirect/notebooks/class_materials/${nbName}.ipynb`;
+  }
+});
