79899e8cea9d0a8b31b9453ff9c4a13806807d15
Aidan Feldman <aidan.feldman@gmail.com>
2022-11-11 13:37:01 -0500

    shortener: don't show the warning on page load

--- _static/shorten.js
+++ _static/shorten.js
@@ -8,6 +8,30 @@ $(() => {
   const activeClasses = "active btn-primary";
   const disabledClasses = "disabled btn-secondary";
 
+  const hideWarning = () => {
+    warningEl.hide();
+  };
+
+  const enableDownload = (url) => {
+    downloadEl
+      .attr("href", url)
+      .addClass(activeClasses)
+      .removeClass(disabledClasses)
+      .removeAttr("aria-disabled");
+
+    hideWarning();
+  };
+
+  const disableDownload = () => {
+    downloadEl
+      .attr("href", "")
+      .addClass(disabledClasses)
+      .removeClass(activeClasses)
+      .attr("aria-disabled", "true");
+
+    warningEl.show();
+  };
+
   const updateDownload = () => {
     const inputText = inputEl.val();
 
@@ -18,31 +42,19 @@ $(() => {
 
     if (matches) {
       // appears to be a Socrata URL
-
       const uid = matches[1];
       const inputUrl = new URL(inputText);
       const numRows = countEl.val();
       const newUrl = `https://${inputUrl.hostname}/resource/${uid}.csv?$limit=${numRows}`;
-
-      downloadEl.attr("href", newUrl);
-      downloadEl
-        .addClass(activeClasses)
-        .removeClass(disabledClasses)
-        .removeAttr("aria-disabled");
-
-      warningEl.hide();
+      enableDownload(newUrl);
     } else {
-      downloadEl.attr("href", "");
-      downloadEl
-        .addClass(disabledClasses)
-        .removeClass(activeClasses)
-        .attr("aria-disabled", "true");
-
-      warningEl.show();
+      disableDownload();
     }
   };
 
   inputEl.on("input", updateDownload);
   countEl.on("input", updateDownload);
   updateDownload();
+  // don't show the warning on page load
+  hideWarning();
 });
