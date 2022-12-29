c771c8367e6774673e84475b7eb0071fa601521f
Aidan Feldman <aidan.feldman@gmail.com>
2022-12-29 00:10:45 -0500

    add some type annotations for Diffable

--- extras/lib/diffable.py
+++ extras/lib/diffable.py
@@ -1,11 +1,12 @@
 from nbconvert.preprocessors import Preprocessor
+from nbformat import NotebookNode
 
 
 def is_system_command(source):
     return source.startswith("!")
 
 
-def is_ipytest(source):
+def is_ipytest(source: str):
     return source.startswith("%%ipytest")
 
 
@@ -13,11 +14,11 @@ def has_html(output):
     return "text/html" in output.get("data", {})
 
 
-def has_html_output(cell):
+def has_html_output(cell: NotebookNode):
     return any(has_html(output) for output in cell["outputs"])
 
 
-def should_clear_output(cell):
+def should_clear_output(cell: NotebookNode):
     """Ignore any system command and ipytest output, since things like package paths shown in warnings/errors can change between different systems. Also clear HTML output, since it often has generated IDs (from displacy, plotly, etc.) that change with each execution."""
     source = cell["source"]
     return is_system_command(source) or is_ipytest(source) or has_html_output(cell)
@@ -26,7 +27,7 @@ def should_clear_output(cell):
 # based off of
 # https://github.com/jupyter/nbconvert/blob/master/nbconvert/preprocessors/tagremove.py
 class Diffable(Preprocessor):
-    def preprocess_cell(self, cell, resources, cell_index):
+    def preprocess_cell(self, cell: NotebookNode, resources, cell_index):
         if cell["cell_type"] != "code":
             return cell, resources
 
