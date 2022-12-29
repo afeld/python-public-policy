f583960bc23836be021b3d312ffe2e2324cb0d19
Aidan Feldman <aidan.feldman@gmail.com>
2022-12-29 11:16:16 -0500

    render schools tempaltes to separate branches in CI

---
+++ extras/scripts/school_ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -x
+
+
+SCHOOL=$1
+
+./extras/scripts/school.sh "$SCHOOL"
+
+# https://lannonbr.com/blog/2019-12-09-git-commit-in-actions/
+# https://github.com/orgs/community/discussions/26560#discussioncomment-3252339
+git config user.name "github-actions[bot]"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+
+TARGET_BRANCH=$SCHOOL-auto
+# create a fresh branch
+git switch -c "$TARGET_BRANCH"
+git add .
+git commit -am "CI: render for school"
+git push -f origin "$TARGET_BRANCH"
