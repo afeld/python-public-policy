a869f96d1b008f8210193efc227f883a710634d5
Aidan Feldman <aidan.feldman@gmail.com>
2023-03-01 01:35:06 -0500

    check absolute links relatively

--- extras/scripts/broken_links.rb
+++ extras/scripts/broken_links.rb
@@ -7,6 +7,8 @@ options = {
   allow_missing_href: true,
   check_external_hash: false,
   only_4xx: true,
-  ignore_files: ['_build/html/_static/webpack-macros.html', '_build/html/_static/sbt-webpack-macros.html']
+  ignore_files: ['_build/html/_static/webpack-macros.html', '_build/html/_static/sbt-webpack-macros.html'],
+  # make absolute links relative
+  swap_urls: { %r{^https://python-public-policy\.afeld\.me/en/\w+} => '' }
 }
 HTMLProofer.check_directory('_build/html', options).run
