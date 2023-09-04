03bbe43ac146c126c0d4b18dec6af5654a7c5d59
Aidan Feldman <aidan.feldman@gmail.com>
2023-09-04 19:03:41 -0400

    use pre-built action for updating branch

--- extras/scripts/school_ci.sh
+++ extras/scripts/school_ci.sh
@@ -27,16 +27,3 @@ fi
 git rm -r extras/scripts/
 
 git diff
-
-if [ "$GITHUB_REF_NAME" = "main" ]; then
-    # https://lannonbr.com/blog/2019-12-09-git-commit-in-actions/
-    # https://github.com/orgs/community/discussions/26560#discussioncomment-3252339
-    git config user.name "github-actions[bot]"
-    git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
-
-    # create a fresh branch locally
-    git switch -c "$SCHOOL"
-    git add .
-    git commit -am "CI: render for school"
-    git push -f origin "$SCHOOL"
-fi
