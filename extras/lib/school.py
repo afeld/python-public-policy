e57fcc808ecdbb7de670e562b915bedbea31ad16
Aidan Feldman <aidan.feldman@gmail.com>
2023-09-28 03:29:11 -0400

    fix build

--- extras/lib/school.py
+++ extras/lib/school.py
@@ -129,6 +129,7 @@ EXEMPT = [
     "nbgrader",
     "nyu's quantitative analysis guide",
     "upload the notebook to [google colab]",
+    "https://community.canvaslms.com/t5/canvas-basics-guide/what-are-grading-schemes/ta-p/41",
     "python coding for public policy assignments",
     "secondary",  # matches "conda"
     "set up the reader",
