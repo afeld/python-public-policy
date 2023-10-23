1520ca0718774038dd58e20d14c94f41907fe845
Aidan Feldman <aidan.feldman@gmail.com>
2023-10-23 17:06:34 -0400

    update link to NYU survey

--- extras/lib/school.py
+++ extras/lib/school.py
@@ -82,7 +82,7 @@ SCHOOLS = [
         lms_url="https://brightspace.nyu.edu/d2l/home/297088",
         lms_anonymous_docs="https://documentation.brightspace.com/EN/le/assignments/instructor/about_anonymous_marking.htm",
         discussions_url="https://brightspace.nyu.edu/d2l/le/297088/discussions/List",
-        survey_url="https://brightspace.nyu.edu/d2l/home/297088",
+        survey_url="https://docs.google.com/forms/d/e/1FAIpQLSe2xspsuAXDTjdFL9fSdC6yZNEnGXuyPt0Yg4YWjWpHRw1CMw/viewform?usp=sf_link",
         lms_notification_settings_url="https://brightspace.nyu.edu/d2l/lms/discussions/admin/subscriptions.d2l?ou=297088",
         lms_reorder_docs="https://community.d2l.com/brightspace/kb/articles/3405-reorder-edit-and-delete-assignments-and-categories",
         coding_env_name="JupyterHub",
