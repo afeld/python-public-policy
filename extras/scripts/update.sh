482d25ea97f19583c30302537cbbd24f190fc4d3
Aidan Feldman <aidan.feldman@gmail.com>
2025-02-24 23:30:37 -0500

    remove execution metadata from cells

--- extras/scripts/update.sh
+++ extras/scripts/update.sh
@@ -9,6 +9,7 @@ jupyter nbconvert \
   --to notebook --inplace --execute --no-prompt \
   --ExecutePreprocessor.kernel_name=python3 \
   --ExecutePreprocessor.timeout=120 \
+  --Exporter.preprocessors=extras.lib.no_execution.NoExecution \
   "$NOTEBOOK"
 
 make format file="$NOTEBOOK"
